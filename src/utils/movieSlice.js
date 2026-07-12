8446dedd4d939adfe83a71409e8e650f2742e59a
Mahesh Patil <maheshkpatil5370@gmail.com>
2026-07-12 09:43:31 +0400

    feat: add usePopularMovies hook and integrate popular movies into Browse and SecondaryContainer components

--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -4,11 +4,15 @@ const moviewSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    popularMovies: null,
     addTrailerVideo: null,
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
+    },
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
     },
     addTrailerVideo: (state, action) => {
       state.addTrailerVideo = action.payload;
@@ -16,5 +20,6 @@ const moviewSlice = createSlice({
   },
 });
 
-export const { addNowPlayingMovies, addTrailerVideo } = moviewSlice.actions;
+export const { addNowPlayingMovies, addPopularMovies, addTrailerVideo } =
+  moviewSlice.actions;
 export default moviewSlice.reducer;
